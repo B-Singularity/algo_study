2d3dac87e389b63a3fd0b8cc74b6e22547c980f8
성경준 <skil1489@naver.com>
2025-02-17 11:53:01 +0900

    add swea_3304

---
+++ 3rd_week/swea_3304.py
@@ -0,0 +1,14 @@
+T = int(input())
+for t in range(1, T + 1):
+    A, B = input().split()
+    dp = [[0] * (len(B) + 1) for _ in range(len(A) + 1)]
+    result = -1
+    for i in range(1, len(A) + 1):
+        for j in range(1, len(B) + 1):
+            if A[i - 1] == B[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+
+    print(f'#{t} {dp[len(A)][len(B)]}')
