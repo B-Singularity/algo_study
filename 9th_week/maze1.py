7d8913a610af616b9c896cddf85c075a9226daca
B-singularity <skil1489@naver.com>
2025-04-13 17:03:12 +0900

    add maze1

--- 9th_week/maze1.py
+++ 9th_week/maze1.py
@@ -13,15 +13,17 @@ for _ in range(10):
     matrix = [list(map(int, list(input().strip()))) for _ in range(16)]
     start_y, start_x = find_start(matrix)
     que = deque()
-    que.append((start_y, start_x))
+    que.append((start_y, start_x, [(start_y, start_x)]))
     visited = [[False] * 16 for _ in range(16)]
     dx = [-1, 1, 0, 0]
     dy = [0, 0, -1, 1]
     flag = False
+    route = []
     while que:
-        y, x = que.popleft()
+        y, x, now_route = que.popleft()
         if matrix[y][x] == 3:
             flag = True
+            route = now_route
             break
 
         visited[y][x] = True
@@ -29,5 +31,6 @@ for _ in range(10):
             ny = y + dy[i]
             nx = x + dx[i]
             if 0 <= ny < 16 and 0 <= nx < 16 and matrix[ny][nx] != 1 and not visited[ny][nx]:
-                que.append((ny, nx))
+                que.append((ny, nx, now_route + [(ny, nx)]))
+    print(f"#{t} {route}")
     print(f'#{t} {1 if flag else 0}')
